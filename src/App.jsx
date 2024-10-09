d5687e857211586a80ae2eefe0668e38d9450bc6
Christina Bürger <christina.buerger@outlook.de>
2024-10-09 14:36:39 +0200

    add color feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,46 @@
+import { useState } from "react";
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
+import ColorForm from "./Components/ColorForm/ColorForm";
 import "./App.css";
+import { uid } from "uid";
 
 function App() {
+  const [colors, setColors] = useState(initialColors);
+
+  function handleAddColor(newColor) {
+    setColors([{ id: uid(), ...newColor }, ...colors]); // Unique ID hinzufügen + neue Farbe am Anfang
+  }
+
   return (
     <>
       <h1>Theme Creator</h1>
 
-      {initialColors.map((color) => {
-        return <Color key={color.id} color={color} />;
-      })}
+      <ColorForm onSubmitColor={handleAddColor} />
+
+      {colors.map((color) => (
+        <Color key={color.id} color={color} />
+      ))}
     </>
   );
 }
 
 export default App;
+
+/*
+
+- Statisch - Farben werden nur einmal beim Laden der Seite aus initialColors geladen (wird nicht aktualisiert)
+- Farben können nicht geändert werden, weil sie nicht in einem Zustand (state) gespeichert sind
+
+return (
+  <>
+    <h1>Theme Creator</h1>
+
+    {initialColors.map((color) => {
+      return <Color key={color.id} color={color} />;
+    })}
+  </>
+);
+}
+
+*/
